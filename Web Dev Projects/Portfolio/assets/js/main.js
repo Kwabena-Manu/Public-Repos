3fb16903d3d9e54e51c91850420fb2834432ca8f
Kwabena Manu <kwabenamanu61@outlook.com>
2024-02-09 10:22:04 -0800

    Add portfolio website

---
+++ Web Dev Projects/Portfolio/assets/js/main.js
@@ -0,0 +1,11 @@
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
+
+
+const lightbox = GLightbox({
+    'href': "https://youtu.be/fpLNgxobiWs?si=rOdSeSoG0TudSyPq",
+    'type': 'video',
+    'source': 'youtube',
+    'width': 900,
+
+
+})
